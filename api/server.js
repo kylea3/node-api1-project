73fc52f614f17d94b8f74b8979733a3306990412
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-04 14:11:10 -0700

    create put api request/response on server.js

--- api/server.js
+++ api/server.js
@@ -29,6 +29,42 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+        User.insert(user)
+            .then(createdUser => {
+                res.status(201).json(createdUser)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "There was an error while saving the user to the database" })
+    })
+    }
+})
+
+server.put('/api/users/:id', async (req, res) => {
+    const user = req.body;
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+    } else {
+    if(!user.name || !user.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+    User.update(req.params.id, req.body)
+        .then(updatedUser => {
+            res.status(200).json(updatedUser)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "The user information could not be modified" })
+        })
+    }
+    }
+})
 
 
 module.exports = server // EXPORT YOUR SERVER instead of {}
