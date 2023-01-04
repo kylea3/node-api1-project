437f7c050b23965652f3ded4b6543389eff365c0
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-04 12:33:31 -0700

    create get request for specific userID in server.js

--- api/server.js
+++ api/server.js
@@ -12,13 +12,22 @@ server.get('/api/users', (req, res) => {
         res.status(200).json(users)
     })
     .catch(err => {
-        res.status(500).json({ 
-            message: `error getting users`,
-            err: err.message
-    })
+        res.status(500).json({ message: "The users information could not be retrieved" })
     })
 })
-      
+  
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        }
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Error getting user" })
+    })
+})
 
 
 
