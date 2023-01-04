3cde408224a586d72b8121c62f747feb91057dd1
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-04 14:30:11 -0700

    create delete request/response on server.js

--- api/server.js
+++ api/server.js
@@ -66,5 +66,20 @@ server.put('/api/users/:id', async (req, res) => {
     }
 })
 
+server.delete('/api/users/:id', async (req, res) => {
+    const possibleUser = await User.findById(req.params.id);
+    if(!possibleUser) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+    } else {
+        User.remove(req.params.id)
+            .then(deletedUser => {
+                res.status(200).json(deletedUser)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "The user could not be removed" })
+            })
+    }
+})
+
 
 module.exports = server // EXPORT YOUR SERVER instead of {}
