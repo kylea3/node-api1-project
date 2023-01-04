33276d5a1a130a96d4efda4d1e2fcf5a3b146598
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-04 12:08:49 -0700

    add get request for /api/users

--- api/server.js
+++ api/server.js
@@ -1,8 +1,25 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
+const User = require('./users/model')
 
 const server = express()
 
 server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({ 
+            message: `error getting users`,
+            err: err.message
+    })
+    })
+})
+      
+
+
 
 module.exports = server // EXPORT YOUR SERVER instead of {}
